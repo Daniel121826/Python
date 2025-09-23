771f4637136db404e96cbf70d8e62a25614a4fbc
Daniel121826 <alumno709866@ies-azarquiel.es>
2025-09-23 16:33:41 +0200

    Ejercicio1_hecho

--- ej1.py
+++ ej1.py
@@ -1,1 +1,10 @@
-pass
+"""Pedimos los datos al usuario"""
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+altura = float(input("¿Cuál es tu altura en metros? "))
+""""Mostramos los datos por pantalla"""
+print(f"Hola {nombre}, tienes {edad} años y mides {altura} metros.")
+"""Vemos el tipo de dato de cada variable"""
+print(type(nombre))
+print(type(edad))
+print(type(altura))
